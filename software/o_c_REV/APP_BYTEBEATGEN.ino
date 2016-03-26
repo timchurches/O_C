4e1ff2bd2a73d5eced2ddbc02c3da4bd65d11d9e
Tim Churches <tim.churches@gmail.com>
2016-03-27 10:50:06 +1100

    [bytebeats] add scaffolding for conditional menu items, but doesn't work for unknown reason

--- software/o_c_REV/APP_BYTEBEATGEN.ino
+++ software/o_c_REV/APP_BYTEBEATGEN.ino
@@ -140,6 +140,56 @@ public:
     return static_cast<uint16_t>(bytebeat_.get_bytepitch()) ; 
   }
 
+  // Begin conditional menu items infrastructure
+  // Maintain an internal list of currently available settings, since some are
+  // dependent on others. It's kind of brute force, but eh, works :) If other
+  // apps have a similar need, it can be moved to a common wrapper
+
+  int num_enabled_settings() const {
+    return num_enabled_settings_;
+  }
+
+  ByteBeatSettings enabled_setting_at(int index) const {
+    return enabled_settings_[index];
+  }
+
+  void update_enabled_settings() {
+    ByteBeatSettings *settings = enabled_settings_;
+    *settings++ = BYTEBEAT_SETTING_EQUATION;
+    *settings++ = BYTEBEAT_SETTING_SPEED;
+    *settings++ = BYTEBEAT_SETTING_P0;
+    *settings++ = BYTEBEAT_SETTING_P1;
+    *settings++ = BYTEBEAT_SETTING_P2;
+    *settings++ = BYTEBEAT_SETTING_LOOP_MODE;
+    if (true == get_loop_mode()) {
+      *settings++ = BYTEBEAT_SETTING_LOOP_START;
+      *settings++ = BYTEBEAT_SETTING_LOOP_START_FINE;
+      *settings++ = BYTEBEAT_SETTING_LOOP_END;
+      *settings++ = BYTEBEAT_SETTING_LOOP_END_FINE;
+    }
+    *settings++ = BYTEBEAT_SETTING_TRIGGER_INPUT;
+    *settings++ = BYTEBEAT_SETTING_STEP_MODE;
+    *settings++ = BYTEBEAT_SETTING_CV1;
+    *settings++ = BYTEBEAT_SETTING_CV2;
+    *settings++ = BYTEBEAT_SETTING_CV3;
+    *settings++ = BYTEBEAT_SETTING_CV4;
+
+    num_enabled_settings_ = settings - enabled_settings_;
+  }
+
+  static bool indentSetting(ByteBeatSettings s) {
+    switch (s) {
+      case BYTEBEAT_SETTING_LOOP_START:
+      case BYTEBEAT_SETTING_LOOP_START_FINE:
+      case BYTEBEAT_SETTING_LOOP_END:
+      case BYTEBEAT_SETTING_LOOP_END_FINE:
+        return true;
+      default: break;
+    }
+    return false;
+  }
+  // end conditional menu items infrastructure
+
   inline void apply_cv_mapping(ByteBeatSettings cv_setting, const int32_t cvs[ADC_CHANNEL_LAST], int32_t segments[kMaxByteBeatParameters]) {
     int mapping = values_[cv_setting];
     uint8_t bytebeat_cv_rshift = 12 ;
@@ -181,26 +231,11 @@ public:
     apply_cv_mapping(BYTEBEAT_SETTING_CV3, cvs, s);
     apply_cv_mapping(BYTEBEAT_SETTING_CV4, cvs, s);
 
-    s[0] = USAT16(s[0]);
-    s[1] = USAT16(s[1]);
-    s[2] = USAT16(s[2]);
-    s[3] = USAT16(s[3]);
-    s[4] = USAT16(s[4]);
-    s[5] = USAT16(s[5]);
-    s[6] = USAT16(s[6]);
-    s[7] = USAT16(s[7]);
-    s[8] = USAT16(s[8]);
-
-    s_[0] = s[0] ;
-    s_[1] = s[1] ;
-    s_[2] = s[2] ;
-    s_[3] = s[3] ;
-    s_[4] = s[4] ;
-    s_[5] = s[5] ;
-    s_[6] = s[6] ;
-    s_[7] = s[7] ;
-    s_[8] = s[8] ;
-        
+    for (uint_fast8_t i = 0; i < 9; ++i) {
+      s[i] = USAT16(s[i]) ;
+      s_[i] = s[i] ;
+    }
+       
     bytebeat_.Configure(s, get_step_mode(), get_loop_mode()) ; 
 
     OC::DigitalInput trigger_input = get_trigger_input();
@@ -225,6 +260,10 @@ private:
   peaks::ByteBeat bytebeat_;
   bool gate_raised_;
   int32_t s_[kMaxByteBeatParameters];
+
+  int num_enabled_settings_;
+  ByteBeatSettings enabled_settings_[BYTEBEAT_SETTING_LAST];
+
 };
 
 void ByteBeat::Init(OC::DigitalInput default_trigger) {
@@ -378,7 +417,16 @@ void BYTEBEATGEN_menu() {
   menu::SettingsList<menu::kScreenLines, 0, menu::kDefaultValueX> settings_list(bytebeatgen.ui.cursor);
   menu::SettingsListItem list_item;
   while (settings_list.available()) {
+// conditional menu items not currently working
+//    const int setting = bytebeat.enabled_setting_at(settings_list.Next(list_item));
+//    const int value = bytebeat.get_value(setting);
+//    const settings::value_attr &attr = ByteBeat::value_attr(setting);
+//    if (ByteBeat::indentSetting(static_cast<ByteBeatSettings>(setting)))
+//          list_item.x += menu::kIndentDx;
+//    list_item.DrawDefault(value, attr);
+
     const int current = settings_list.Next(list_item);
+    // const int current = bytebeat.enabled_setting_at(settings_list.Next(list_item));
     list_item.DrawDefault(bytebeat.get_value(current), ByteBeat::value_attr(current));
   }
 }
